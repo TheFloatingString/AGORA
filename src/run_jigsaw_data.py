1a22bc185dacb36f89ef477cd9303b4eb94b6b07
The Floating String <laurencel2001@gmail.com>
2022-12-27 20:02:22 -0500

    Implement Jigsaw classification

--- src/run_jigsaw_data.py
+++ src/run_jigsaw_data.py
@@ -6,7 +6,35 @@ TEST_FILEPATH = "data/jigsaw-dataset/test.csv"
 
 train_df = pd.read_csv(TRAIN_FILEPATH)
 print(train_df.head(5))
+print(train_df.shape)
 
-agora_obj = Agora()
-resp = agora_obj.contains_jigsaw_hate_speech("Unblock me or I'll get my lawyers on to you for blocking my constitutional right to free speech")
-print(resp)
+
+results_df = pd.DataFrame()
+
+pred_dict_map = {
+        False: 0,
+        True: 1
+        }
+
+
+for i in range(0, 100):
+    agora_obj = Agora()
+    input_text = train_df.iloc[i]["comment_text"]
+
+    agora_pred = agora_obj.contains_jigsaw_hate_speech(input_text)
+    print(i, agora_pred)
+
+    temp_results_dict = {
+            "id": train_df.iloc[i]["id"],
+            "toxic": pred_dict_map[agora_pred["toxic"]],
+            "severe toxic": pred_dict_map[agora_pred["severe toxic"]],
+            "obscene": pred_dict_map[agora_pred["obscene"]],
+            "threat": pred_dict_map[agora_pred["threat"]],
+            "insult": pred_dict_map[agora_pred["insult"]],
+            "identity hate": pred_dict_map[agora_pred["identity hate"]]
+            }
+
+    results_df = results_df.append(temp_results_dict, ignore_index=True)
+
+results_df.to_csv("results.csv", index=False)
+
