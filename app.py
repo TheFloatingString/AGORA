21aeeeec9a6102f3a3c4f09979fb79e3e163ed6b
The Floating String <laurencel2001@gmail.com>
2023-03-03 21:40:36 -0500

    Add app.py with CORS enabled

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, Request
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
+
+origins = [
+        "*"
+        ]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+        )
 
 @app.get("/")
 async def root():
     return {"data": "McGill AI Society Project X, 2022-2023"}
+
+
+@app.post("api/upload")
+async def api_upload(request: Request):
+    form = await request.form()
+    filename = form["upload_file"].filename
+    contents = await form["upload_file"].read()
+    with open(filename, "wb") as f:
+        f.write(contents)
+    return {"data": "Completed"}
+
+@app.post("/api/transcribe")
+async def api_transcribe(request: Request):
+    form = await request.form()
+    print(form)
+    print(dir(form))
+    return {"data": "received"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080)
