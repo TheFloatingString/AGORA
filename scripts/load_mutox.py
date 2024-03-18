376da59b47d65ae54b520d4deea998b61da8d18d
TheFloatingString <laurence.w.liang@gmail.com>
2024-03-18 18:33:15 +0000

    WIP

--- scripts/load_mutox.py
+++ scripts/load_mutox.py
@@ -1,19 +1,13 @@
 import requests
 import pandas as pd
 from pydub import AudioSegment
+from tqdm import trange
 import os
 
-N_SAMPLES = 10
-
+N_SAMPLES = 200
+VERBOSE = False
 
 df = pd.read_csv("data/mutox/mutox.tsv", delimiter='\t')
-print(df.columns.values)
-print(df.head())
-location_list = df['public_url_segment'][0].split(' ')
-public_url = location_list[0]
-start = location_list[1]
-end = location_list[2]
-print(public_url, start, end)
 
 def return_info_from_str(info_str):
     return_dict = dict()
@@ -23,11 +17,12 @@ def return_info_from_str(info_str):
     return_dict['end'] = int(location_list[2])
     return return_dict
 
-for i in range(N_SAMPLES):
+for i in trange(N_SAMPLES):
     sample_id = df['id'][i]
     info_str = df['public_url_segment'][i]
     info_dict = return_info_from_str(info_str)
-    print(info_dict)
+    if VERBOSE:
+        print(info_dict)
     audio_resp = requests.get(info_dict['public_url'])
 
     with open('tmp.mp3', 'wb') as f:
@@ -35,7 +30,7 @@ for i in range(N_SAMPLES):
 
     audio = AudioSegment.from_mp3('tmp.mp3')
     cropped_audio = audio[info_dict['start']:info_dict['end']]
-    cropped_audio.export(f'mutox-{sample_id}.wav',format='wav')
+    cropped_audio.export(f'data/mutox/Data/mutox-{sample_id}.wav',format='wav')
     
     os.remove('tmp.mp3')
-    break
+    
